62d21f43a0d3f006eef56fe478168981373be231
iluvwaffles2 <iluvwaffles4@gmail.com>
2019-10-05 00:04:10 -0400

    Added enemy movement

--- Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
+++ Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    float health;
-    float attackDamage;
-    float attackRate;
-    float attackRange;
-    float speed;
+    public float health;
+    public float attackDamage;
+    public float attackRate;
+    public float attackRange;
+    public float movementRange;
+    public float speed;
+    float wallCoord = -6.85F;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x - wallCoord >= movementRange)
+        {
+            Vector3 temp = new Vector3(speed, 0, 0);
+            transform.position -= temp;
+        }
+        if (transform.position.x - wallCoord >= attackRange)
+        {
+
+        }
     }
 }
