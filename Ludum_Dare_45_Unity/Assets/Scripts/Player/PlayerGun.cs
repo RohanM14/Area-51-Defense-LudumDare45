e046ae7f4132465d97f3c772bad93ca6ec1848e9
kjbowman00 <kjbowman00@vt.edu>
2019-10-04 22:17:25 -0400

    Added crosshair around mouse position

--- Ludum_Dare_45_Unity/Assets/Scripts/Player/PlayerGun.cs
+++ Ludum_Dare_45_Unity/Assets/Scripts/Player/PlayerGun.cs
@@ -6,17 +6,32 @@ public class PlayerGun : MonoBehaviour
 {
     float damage;
     float fireRate;
+
+    Camera cam;
+    GameObject crosshair;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
+        crosshair = GameObject.Find("Crosshair");
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Locate mouse location.
+        //Find mouse location
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = cam.ScreenToWorldPoint(mousePos);
+        mousePos.z = -1;
+        //Update crosshair
+        crosshair.transform.position = mousePos;
+        Debug.Log(mousePos);
+        mousePos.z = 0;
+
         //find the angle player gun should be facing
+        Vector2 lookDir = transform.position - mousePos;
+
         //Clamp it to a certain range so that you can't shoot backwards
         //change the angle
     }
