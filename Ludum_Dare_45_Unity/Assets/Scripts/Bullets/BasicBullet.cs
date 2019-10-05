2e67bec5de9adbbe632ff14610aadd71ce27fb69
iluvwaffles2 <iluvwaffles4@gmail.com>
2019-10-05 01:02:06 -0400

    added destroy function to bullets and gave enemy script the ability to shoot a bullet.

--- Ludum_Dare_45_Unity/Assets/Scripts/Bullets/BasicBullet.cs
+++ Ludum_Dare_45_Unity/Assets/Scripts/Bullets/BasicBullet.cs
@@ -10,7 +10,7 @@ public class BasicBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, timeToDie);
     }
 
     private void FixedUpdate()
--- Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
+++ Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
@@ -10,6 +10,8 @@ public class Enemy : MonoBehaviour
     public float attackRange;
     public float movementRange;
     public float speed;
+    private float shotTimer = 0;
+    public GameObject bullet;
     float wallCoord = -6.85F;
 
     // Start is called before the first frame update
@@ -21,14 +23,29 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moves the enemy left at speed.
         if (transform.position.x - wallCoord >= movementRange)
         {
             Vector3 temp = new Vector3(speed, 0, 0);
             transform.position -= temp;
         }
-        if (transform.position.x - wallCoord >= attackRange)
-        {
 
+        // if within firing range, starts firing.
+        if (transform.position.x - wallCoord < attackRange)
+        {
+            handleShooting();
+        }
+    }
+
+    private void handleShooting()
+    {
+        shotTimer += Time.deltaTime;
+        if (shotTimer >= attackRate)
+        {
+            shotTimer = 0;
+            //Shoot a bullet
+            Debug.Log(transform.rotation);
+            Instantiate(bullet, transform.position, Quaternion.Euler(0,0,transform.rotation.eulerAngles.z-180));
         }
     }
 }
