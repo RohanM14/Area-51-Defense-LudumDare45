5abe6cfc8544c49532f50c09774cb187fab32f14
kjbowman00 <kjbowman00@vt.edu>
2019-10-04 21:38:24 -0400

    Setup some basic files

---
+++ Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float health;
+    float attackDamage;
+    float attackRate;
+    float attackRange;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ludum_Dare_45_Unity/Assets/Scripts/Player/PlayerGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour
+{
+    float damage;
+    float fireRate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Locate mouse location.
+        //find the angle player gun should be facing
+        //Clamp it to a certain range so that you can't shoot backwards
+        //change the angle
+    }
+}
