9c2a44e351811e4cf5df777dc5e4324099c8928a
rohanm14 <rohanm14@vt.edu>
2019-10-05 22:34:42 -0400

    Upgrade basics

---
+++ Ludum_Dare_45_Unity/Assets/Scripts/Player/UpgradeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    int money;
+    int[] upgrades = new int[6];
+    int wallLevel;
+
+    public int[] getUpgrades()
+    {
+        return upgrades;
+    }
+
+    public int getWall()
+    {
+        return wallLevel;
+    }
+
+    public void upgradeWall()
+    {
+        wallLevel++;
+    }
+    public void upgrade(int slot)
+    {
+        upgrades[slot] = upgrades[slot] + 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
