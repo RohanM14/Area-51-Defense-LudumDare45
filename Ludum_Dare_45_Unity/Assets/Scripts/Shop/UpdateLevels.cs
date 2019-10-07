20b7eaf6eb0cf6bed0c7121d3387222ad501d339
kjbowman00 <kjbowman00@vt.edu>
2019-10-07 04:55:33 -0400

    Fixed shop issue. Circles weren't displaying when returning

--- Ludum_Dare_45_Unity/Assets/Scripts/Shop/UpdateLevels.cs
+++ Ludum_Dare_45_Unity/Assets/Scripts/Shop/UpdateLevels.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class UpdateLevels : MonoBehaviour
 {
-    public static Sprite[] sprites;
+    public Sprite[] sprites;
     // Start is called before the first frame update
     void Start()
     {
         int upgradeNum = int.Parse(gameObject.name.Substring(12, 1));
         Image image = GetComponent<Image>();
-        //image.sprite = sprites[UpgradeManager.GetUpgrades()];
+        image.sprite = sprites[UpgradeManager.GetUpgrades()[upgradeNum-1]];
     }
 
     // Update is called once per frame
