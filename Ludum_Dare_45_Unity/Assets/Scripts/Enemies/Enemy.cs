926dbdb464e0739e751cd04f7f43093308a05e4c
kjbowman00 <kjbowman00@vt.edu>
2019-10-06 22:36:37 -0400

    Enemy death skeleton

--- Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
+++ Ludum_Dare_45_Unity/Assets/Scripts/Enemies/Enemy.cs
@@ -4,42 +4,79 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int cashValue;
     public float health;
     public float attackDamage;
     public float attackRate;
     public float attackRange;
     public float movementRange;
     public float speed;
-    public class damage
-    {
-        public float attackDamage;
-    }
 
     private float shotTimer = 0;
+    private bool alive;
     public GameObject bullet;
     float wallCoord = -6.85F;
+
+    private Animator animator;
+    private SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Moves the enemy left at speed.
-        if (transform.position.x - wallCoord >= movementRange)
+        if (alive)
         {
-            Vector3 temp = new Vector3(speed, 0, 0);
-            transform.position -= temp;
-        }
+            // Moves the enemy left at speed.
+            if (transform.position.x - wallCoord >= movementRange)
+            {
+                Vector3 temp = new Vector3(speed, 0, 0);
+                transform.position -= temp;
+            }
 
-        // if within firing range, starts firing.
-        if (transform.position.x - wallCoord < attackRange)
-        {
-            handleShooting();
+            // if within firing range, starts firing.
+            if (transform.position.x - wallCoord < attackRange)
+            {
+                handleShooting();
+            }
         }
+        else
+        {
+            //Handle death animation
+            float otherValues = Mathf.Lerp(sprite.color.b, 0, 0.1f);
+            sprite.color = new Color(256, otherValues, otherValues);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, -90), 0.2f);
+
+            //When animation is done, Destroy object
+        }
+        
+    }
+
+    public void hurt(int damage)
+    {
+        health -= damage;
+        if (health <= 0) kill();
+    }
+
+    private void kill()
+    {
+        //Add money
+
+        //Disable movement
+        //Disable attacks
+        //Death animation
+        GetComponent<Animator>().StopPlayback();
+        alive = false;
+
+        //Disable trigger boxes
+        //GetComponent<Collider2D>().disable
+
     }
 
     private void handleShooting()
@@ -51,6 +88,16 @@ public class Enemy : MonoBehaviour
             //Shoot a bullet
             Instantiate(bullet, transform.position, Quaternion.Euler(0,0,transform.rotation.eulerAngles.z-180)).tag = "BasicBullet";
             
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FriendlyBullet")
+        {
+            BasicBullet bullet = collision.GetComponent<BasicBullet>();
+            //hurt(bullet.damage);
+            Destroy(collision);
         }
     }
 }
